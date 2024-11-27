64f9a98bc11a7975c34e6e4c5fca889905cc6ebc
Eman Gaber <emangaber413@gmail.com>
2024-11-27 17:27:40 +0200

    first commit

---
+++ pattern.js
@@ -0,0 +1,48 @@
+
+
+// function button1func {
+// for ( var x=1 ; x* '*'
+//       )
+
+// }
+
+// function button1func {var x="*" *  2}
+//  console.log(x);
+var rows =5
+var cols=5
+
+
+for(var x=1;x<=rows;x++){
+// document.write("*") 
+for(var y=1;y<=x;y++){
+    document.write("*") 
+   
+    }
+    document.write('<br>')
+}
+
+
+
+document.write("<br>")
+
+for(var i=1;i<=rows;i++){
+// document.write("*") 
+for(var k=5;k>=i;k--){
+    document.write("*") 
+   
+    }
+    document.write('<br>')
+}
+
+
+
+
+
+
+
+
+// var x=1
+
+// document.getElementById('pattern2').innerText=x
+
+console.log(x);
